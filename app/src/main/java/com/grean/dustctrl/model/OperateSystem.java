84a7e8e99ff4eea5dcdabd579b923fb432efe4f2
lethe12 <weifeng_apex@126.com>
2017-09-05 10:20:24 +0800

    增加手动更新功能，并测试

--- app/src/main/java/com/grean/dustctrl/model/OperateSystem.java
+++ app/src/main/java/com/grean/dustctrl/model/OperateSystem.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Environment;
-import android.os.Handler;
 import android.os.Process;
 import android.util.Log;
 import com.grean.dustctrl.CtrlCommunication;
